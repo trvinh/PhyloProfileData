bcd9b16410f32c0669e154240d3628fe37a12c30
Vinh Tran <tran@bio.uni-frankfurt.de>
2019-05-25 02:09:05 +0200

    Update PhyloProfileData.R

--- R/PhyloProfileData.R
+++ R/PhyloProfileData.R
@@ -2,7 +2,7 @@
 #' 
 #' @description The PhyloProfileData package provides a collection of datasets 
 #' to accompany the R package PhyloProfile, where they are used to illustrate 
-#' how to run MOFA and analyse its results. Briefly, it contains the 
+#' how to run PhyloProfile and analyse its results. Briefly, it contains the 
 #' phylogenetic profiles, the fasta sequences and the domain annotations for two
 #' experimental data sets, including human AMPK-TOR pathway proteins and BUSCO 
 #' Arthropoda proteins. In addition, a preprocessing NCBI taxonomy data is also
